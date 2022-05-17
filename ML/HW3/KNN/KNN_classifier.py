c1010b3e49ddf6fb3e087396d8a54ccbbb264e8c
jongbinny98 <jongbinny98@gmail.com>
2022-05-16 17:11:53 -0700

    updated

--- ML/HW3/KNN/KNN_classifier.py
+++ ML/HW3/KNN/KNN_classifier.py
@@ -6,50 +6,57 @@ from math import sqrt
 def euclidean_distance(x1, x2):
   return np.sqrt(np.sum(x1 - x2)**2)  
 
-def find_neighbors(x_train, test, k):
+# To find the k neighbor's label
+def find_neighbors(x_train, y_label, test, k):
 
+  # Predict neighbor = k-th closest neighbors
+  Predict_Neighbors = np.zeros((k))
 
-  
+  # temp = store the distance btw x_train and test[0] point
+  temp = np.zeros((len(x_train)))
+  distance = np.zeros((len(x_train)))
+  temp = np.zeros((len(x_train)))
+
+  # find the distance and use index to find the k-th closest neighbors
+  for i in range(len(x_train)):
+    for j in range(len(test)):
+      distance[j] = euclidean_distance(x_train[i], test[j])
+      temp[i] = distance[j]
+  print(temp)
+
+  # get the index of temp 
+  index = temp.argsort()
+  print(index)
+  # find the k-th closest label
+  for j in range(k):
+    Predict_Neighbors[j] = index[j]
+    
+  return Predict_Neighbors
 
 def run(Xtrain_file, Ytrain_file, test_data_file, pred_file): 
-  '''The function to run your ML algorithm on given datasets, generate 
-  the predictions and save them into the provided file path 
 
-  Parameters 
-  ---------- 
-  Xtrain_file: string 
-     Each row is a feature vector. The values in the -th columns are float numbers in the -th dimension
-  Ytrain_file: string 
-    The CSV file provides the multi-class labels for corresponding feature vectors in the file Xtrain.csv .
-    Please note the labels will be integer numbers between 0 and 10. 
-  test_data_file: string 
-    the path to test data csv file 
-  pred_file: string 
-    the prediction file name to be saved by your code.
-  Ytrain_file 
-  ''' 
-  # read data from Xtrain_file, Ytrain_file.
   # read data from Xtrain_file, Ytrain_file.
   x = np.loadtxt(Xtrain_file, delimiter=',')
   y = np.loadtxt(Ytrain_file, delimiter=',')
 
+  #split the data into train(0.90) and test(0.10)
   train_test_split = int(0.9 * len(x))
   x_train, y_label = x[:train_test_split], y[:train_test_split]
   test, test_label = x[train_test_split:], y[train_test_split:]
-  
-  test =  test[0]
-  for i in range(len(x_train)):
-    distance = euclidean_distance(x_train[i], test)
-    print(distance)
-  
-  # find the closest neighbors 3
-  k = 3
-  
-  # find the neighbor 
-  neighbors = find_neighbors(x_train, test, k)
-  for n in range(len(neighbors)):
-    print(neighbors[n])
 
+  # set k-th closest
+  k = 3
+
+  # find the k-th closest neighbors label
+  neighbors_label = find_neighbors(x_train, y_label, test, k)
+
+  print("neighbor: ", neighbors_label)
+  
+  # prediction
+  # prediction = np.zeros(len(test))  
+  # for i in range(len(test)):
+  #   if 
+  
 
   # save pred_file
   # np.savetxt(pred_file, prediction, fmt='%1d', delimiter=",")
