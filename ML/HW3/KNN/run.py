0c69cdc936976413d9dad4ca53d353d551f0d31b
jongbinny98 <jongbinny98@gmail.com>
2022-05-17 19:00:29 -0700

    updated

--- ML/HW3/KNN/run.py
+++ ML/HW3/KNN/run.py
@@ -30,45 +30,45 @@ def find_neighbors(x_train, y_label, test_data_file, k):
 def run(Xtrain_file, Ytrain_file, test_data_file, pred_file): 
 
   # read data from Xtrain_file, Ytrain_file.
-  x = np.loadtxt(Xtrain_file, delimiter=',')
-  y = np.loadtxt(Ytrain_file, delimiter=',')
+  x_train = np.loadtxt(Xtrain_file, delimiter=',')
+  y_label = np.loadtxt(Ytrain_file, delimiter=',')
 
-  #split the data into train(0.90) and test(0.10)
-  train_test_split = int(0.9 * len(x))
-  x_train, y_label = x[:train_test_split], y[:train_test_split]
-  test, test_label = x[train_test_split:], y[train_test_split:]
-
-  #store test into test_data_file
-  test_data_file = test
+  # read data from test_data_file(Xtrain.csv)
+  test = np.loadtxt(test_data_file, delimiter=',')
 
   # set k-th closest
-  k = 3
+  k = 2
 
   # array of neighbors labels(rough before applying majority rule)
   neighbors = []
-  for i in range(len(test_data_file)):
-    test_value = test_data_file[i, :]
+  for i in range(len(test)):
+    test_value = test[i, :]
     labels = find_neighbors(x_train, y_label, test_value, k)
     neighbors.append(labels)
 
-  prediction = np.zeros(len(test_data_file))
+  prediction = np.zeros(len(test))
   temp = np.zeros(0)
 
-  for i in range(len(test_data_file)):
+  for i in range(len(test)):
     label, count = np.unique(neighbors[i], return_counts = True)
     temp = label[np.argmax(count)]
     prediction[i] = temp
-  print("prediction =", prediction == test_label)
+
+  #test
+  print("....", neighbors[1])
+  print("prediction: ", prediction)
+  print("y_label: ", y_label)
+
+  print("prediction =", prediction == y_label)
 
   # save pred_file
   np.savetxt(pred_file, prediction, fmt='%1d', delimiter=",")
 
-# define other functions here
 if __name__ == '__main__':
 
   Xtrain_file = "Xtrain.csv"
   Ytrain_file = "Ytrain.csv"
-  test_data_file = None
+  test_data_file = "Xtrain.csv"
   pred_file = 'result'
 
   run(Xtrain_file, Ytrain_file, test_data_file, pred_file)
