c805f00570f49064d914540b4343b273a1e6f2ec
jongbinny98 <jongbinny98@gmail.com>
2022-05-30 20:26:02 -0700

    updated

--- ML/HW4/boostit.py
+++ ML/HW4/boostit.py
@@ -1,19 +1,69 @@
-import numpy as np
-import math
 
+import numpy as np
+
+'''
+This is basic linear classifier from hw 2
+'''
 class LinearClassifier:
 
     def __init__(self):
-    
-        self.t = []  # threshold for class prediction
-        self.w = []          # orthogonal vector to threshold 
+        # initialize threshold and w vector
+        self.t = []  
+        self.w = [] 
 
+    # find the centroid for each class -1,1
+    def class_exemplar(self, w, x):
+        # instead of using mean(), use the equation 1/sum(w) * sum(w*x)
+        centroid = np.sum(x, axis = 0) / np.sum(w)
+
+        return centroid
+
+    # find the error rate
+    def error(self, X, y, weights):
+        
+        pred = self.predict(X)
+        
+        TP = 0
+        FN = 0
+        TN = 0
+        FP = 0
+        # confusion matrix form local_evaluation.py
+        for i in range(len(y)):
+            pred_label = pred[i]
+            gt_label = y[i]
+
+            if int(pred_label) == -1:
+                if pred_label == gt_label:
+                    TN += 1 * weights[i]
+                else:
+                    FN += 1 * weights[i]
+            else:
+                if pred_label == gt_label:
+                    TP += 1 * weights[i]
+                else:
+                    FP += 1 * weights[i]
+        # calculation
+        accuracy = (TP + TN) / (TP + FN + FP + TN)
+        precision = TP / (TP + FP) if ((TP + FP) > 0) else 0
+        recall = TP / (TP + FN) if ((TP + FN)) > 0 else 0
+        f1 = 2 * precision * recall / (precision + recall) if ((precision + recall) > 0) else 0
+        final_score = 50 * accuracy + 50 * f1
+        # error rate
+        err = 1 - ( (TP + TN) / (TP + FN + FP + TN) )
+
+        print("accuracy: ", accuracy)
+        print("precision: ", precision)
+        print("final score: ", final_score)
+
+        return err, pred
+
+    # train
     def train(self, X, y, weights):
-
+        
         n_features = X.shape[1]
         n_examples = X.shape[0]
     
-        x = np.zeros(shape = (n_examples, n_features))
+        x = np.zeros((n_examples, n_features))
 
         # X * weights
         for i in range(n_examples):
@@ -48,6 +98,7 @@ class LinearClassifier:
 
         return self
 
+    # prediction
     def predict(self, X):
         # print(X)
         n_samples = X.shape[0]
@@ -63,72 +114,41 @@ class LinearClassifier:
         # print(pred)
         return pred
 
-    def error(self, X, y, weights):
-        
-        pred = self.predict(X)
-        
-        TP = 0
-        FN = 0
-        TN = 0
-        FP = 0
+'''
+boosting persudo code 5-24.17 - train an ensemble of binary classifier from reweighted training set 
 
-        for i in range(len(y)):
-            pred_label = pred[i]
-            gt_label = y[i]
-
-            if int(pred_label) == -1:
-                if pred_label == gt_label:
-                    TN += 1 * weights[i]
-                else:
-                    FN += 1 * weights[i]
-            else:
-                if pred_label == gt_label:
-                    TP += 1 * weights[i]
-                else:
-                    FP += 1 * weights[i]
-
-        accuracy = (TP + TN) / (TP + FN + FP + TN)
-        precision = TP / (TP + FP) if ((TP + FP) > 0) else 0
-        recall = TP / (TP + FN) if ((TP + FN)) > 0 else 0
-        f1 = 2 * precision * recall / (precision + recall) if ((precision + recall) > 0) else 0
-        final_score = 50 * accuracy + 50 * f1
-        err = 1 - ( (TP + TN) / (TP + FN + FP + TN) )
-        print("accuracy: ",accuracy)
-        print("precision: ", precision)
-        print("final score: ", final_score)
-        return err, pred
-
-    def class_exemplar(self, w, x):
-        centroid = np.sum(x, axis = 0) / np.sum(w)
-        return centroid
-
+input - data set X, ensemble size T, learning algorithm A(linear classifier)
+output - weighted ensemble of models
+'''
 class BoostingClassifier:
 
     def __init__(self):
-        
-        self.T = 5
-        self.A = LinearClassifier
 
-        # create an empty list of models
+        # Ensemble size
+        self.T = 5
+        # learning algorithm(Linear Classifier)
+        self.A = LinearClassifier
+        # model that need to be output 
         self.M = [None for i in range(self.T + 1)]
+        # confidence for this model
         self.alpha = [None for i in range(self.T + 1)]
 
+    # train
     def fit(self, X, y):
-
-        n_examples = X.shape[0]   # number of samples in training set
-
-        w = np.zeros(shape=(self.T+1, n_examples))
-        # print(w.shape)
-        # print(w[1])
-        # print("-----------------------------------------------")
-
-        w[1] = np.array([1 / n_examples for i in range(n_examples)])  # initialize weights 
-        # print(w[1])
-        # iterate over T training instances
+        print("M:", self.M)
+        print("alpha: ", self.alpha)
+        print("________________________________")
+        n_examples = X.shape[0]
+        w = np.zeros((self.T + 1, n_examples))
+        
+        # start from uniform weight w1i = 1/dataset
+        w[1] = np.array([1 / n_examples for i in range(n_examples)])
+        
+        # run algorithm on data X with weight wti to produce a model Mt
         for t in range(1, self.T + 1):
 
             if True:
-                print("\nIteration " + str(t) + ":")
+                print(f"\nIteration {t}:")
 
             # find the model that has trained 
             self.M[t] = self.A().train(X, y, w[t])  
@@ -136,42 +156,43 @@ class BoostingClassifier:
             error, pred = self.M[t].error(X, y, w[t])    
             
             if True:
-                print("Error = " + str(error))
+                print(f"Error = {error}")
             
             # if err is more than half, then it will harm it so break
             if error >= 1/2:                   
                 self.T = t - 1
                 break;
 
-            # compute the alpha
-            self.alpha[t] = (1/2) * math.log((1 - error) / error)  
+            # compute the confidence of the model
+            self.alpha[t] = (1/2) * np.log((1 - error) / error)  
             
             if True:
-                print("Alpha = " + str(self.alpha[t]))
+                print(f"Alpha = {self.alpha[t]}")
 
-                f_inc = 1 / (2 * error)
-                f_dec = 1 / (2 * (1 - error))
+                # to print factor to increase and decrease
+                factor_increase = 1 / (2 * error)
+                factor_decrease = 1 / (2 * (1 - error))
 
-                print("Factor to increase weights = " + str(f_inc))
-                print("Factor to decrease weights = " + str(f_dec))
+                print(f"Factor to increase weights = {factor_increase}")
+                print(f"Factor to decrease weights = {factor_decrease}")
 
             # increase or decrease the weights
-            if t != self.T:
+            if t < self.T:
                 for i in range(n_examples):
                     # for misclassified instances
-                    if y[i] * pred[i] < 0:  
+                    if (y[i] * pred[i]) < 0:  
                         w[t+1][i] = w[t][i] / (2 * error)
                     # for correctly classified instances
                     else:                               
                        w[t+1][i] = w[t][i] / (2 * (1 - error))
-
         return self
 
     # prediction
     def predict(self, X):
 
-        prediction = np.zeros(shape = (X.shape[0], 1))
+        prediction = np.zeros((X.shape[0], 1))
 
+        # return np.sum of alpha Mx
         for i in range(1, self.T):
             prediction += self.alpha[i] * self.M[i].predict(X)
            
